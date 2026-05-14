c1588882fea8e90c6c8c44d432652ca0b81d83b6
navaneethan0312 <s.navaneethan0312@gamil.com>
2026-05-14 11:59:46 +0000

    last time

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
   if (req.url === '/health') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ status: 'ok' }));
+    res.writeHead(200);
+    res.end('OK');
     return;
   }
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Node App Deployed Successfully</h1>');
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Node App Running</h1>');
 });
 
-// Only start listening when not in test mode
-if (require.main === module) {
-  server.listen(3000, () => {
-    console.log('Server running on port 3000');
-  });
-}
-
+server.listen(process.env.PORT || 3000);
 module.exports = server;
